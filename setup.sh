5f198b2b65d106925281de6f71d23bbe7c1c4ad9
d-lugs <dclustig28@gmail.com>
2026-02-06 09:22:03 -0500

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# get absolute path of script
+path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+
+# pull dotfiles/dirs
+filelist=$(ls -l $path | grep '^-' | grep -vP "(README.md|setup.sh)" | awk '{print $NF}')
+for file in $filelist; do
+    cp $file "~./$file"
+done
+
+dirlist=$(ls -l $path | grep '^d' | awk '{print $NF}')
+for dir in $dirlist; do
+    cp -pR $dir "~/.config/"
+done
+
+# source dotfiles
+. ~/.bashrc
