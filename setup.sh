35d9274b785af249c3a8670e93a3a102e7504e5c
d-lugs <dclustig28@gmail.com>
2026-02-06 15:54:46 -0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 
 # get absolute path of script
 path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
+cd $path
 
 # pull dotfiles/dirs
 filelist=$(ls -l $path | grep '^-' | grep -vP "(README.md|setup.sh)" | awk '{print $NF}')
 for file in $filelist; do
-    cp $file "~./$file"
+    cp $file "~./.$file"
 done
 
 dirlist=$(ls -l $path | grep '^d' | awk '{print $NF}')
@@ -15,4 +16,5 @@ for dir in $dirlist; do
 done
 
 # source dotfiles
+cd
 . ~/.bashrc
