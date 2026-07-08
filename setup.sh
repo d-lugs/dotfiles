60adde95b3263e5df58123944b814670d318ae5a
d-lugs <insert email here>
2026-07-08 19:49:26 -0400

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,20 +4,46 @@
 path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 cd $path
 
+user_confirm(){
+    while true; do
+        read -p "Update $1? (y/n) " yn
+        case $yn in
+            [Yy]*) return 0 ;;
+            [Nn]*) return 1 ;;
+        esac
+    done
+}
+
+write_config(){
+    if $(user_confirm $1); then
+        cp -vrf $1 $2
+    else
+        echo "Skipping $1...";
+    fi
+}
+
 # pull dotfiles/dirs
 filelist=$(ls -l $path | grep '^-' | grep -vP "(README.md|setup.sh)" | awk '{print $NF}')
 for file in $filelist; do
-    cp -f $file ~/.$file
+    if cmp -s -- $file ~/.$file && [ -e ~/.$file ]; then
+        continue
+    elif [ -e ~/.$file ]; then
+        echo "Changes to $file found."
+    fi
+    write_config $file ~/.$file
 done
 
 dirlist=$(ls -l $path | grep '^d' | awk '{print $NF}')
 for dir in $dirlist; do
-    if [ ! -d ~/.config ]; then
-        mkdir ~/.config
+    if [ $(diff -r $dir ~/.config/$dir 2>/dev/null | wc -l) -eq 0 ] && [ -d ~/.config/$dir ]; then
+        continue
+    elif [ -d ~/.config/$dir ]; then
+        echo "Changes to $dir found."
     fi
-    cp -pR $dir ~/.config/$dir
+    write_config $dir ~/.config/$dir
 done
 
 # source dotfiles
-cd
 . ~/.bashrc
+
+echo "Dotfiles are up to date."
