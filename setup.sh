31e7fae90482a919f6ab7719f73bd13faa2e5a01
d-lugs <insert email here>
2026-07-09 12:18:18 -0400

    fixing user input

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
 
-# get absolute path of script
-#path=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
-#cd $path
-
 # pull latest dotfiles into a temp directory
 path="/tmp/dotfiles"
 rm -rf $path
@@ -11,7 +7,7 @@ git clone -q https://github.com/d-lugs/dotfiles.git $path
 
 user_confirm(){
     while true; do
-        read -p "Update $1? (y/n) " yn
+        read -ep "Update $1? (y/n) " yn
         case $yn in
             [Yy]*) return 0 ;;
             [Nn]*) return 1 ;;
@@ -20,32 +16,36 @@ user_confirm(){
 }
 
 write_config(){
-    if $(user_confirm $1); then
-        cp -vrf $1 $2
-    else
-        echo "Skipping $1...";
-    fi
+    diff $1 $2
+    while true; do
+        read -p "Update $1? (y/n) " yn </dev/tty
+        case $yn in
+            [Yy]*) cp -vrf $1 $2 ; return 0 ;;
+            [Nn]*) echo "Skipping $1..." ; return 0 ;;
+        esac
+    done
+    return
 }
 
 # pull dotfiles/dirs
 filelist=$(ls -l $path | grep '^-' | grep -vP "(README.md|setup.sh)" | awk '{print $NF}')
 for file in $filelist; do
-    if cmp -s -- $file ~/.$file && [ -e ~/.$file ]; then
+    if cmp -s -- $path/$file ~/.$file && [ -e ~/.$file ]; then
         continue
     elif [ -e ~/.$file ]; then
         echo "Changes to $file found."
     fi
-    write_config $file ~/.$file
+    write_config $path/$file ~/.$file
 done
 
 dirlist=$(ls -l $path | grep '^d' | awk '{print $NF}')
 for dir in $dirlist; do
-    if [ $(diff -r $dir ~/.config/$dir 2>/dev/null | wc -l) -eq 0 ] && [ -d ~/.config/$dir ]; then
+    if [ $(diff -r $path/$dir ~/.config/$dir 2>/dev/null | wc -l) -eq 0 ] && [ -d ~/.config/$dir ]; then
         continue
     elif [ -d ~/.config/$dir ]; then
         echo "Changes to $dir found."
     fi
-    write_config $dir ~/.config/$dir
+    write_config $path/$dir ~/.config/$dir
 done
 
 # source dotfiles
